e288d84082a1a958720eb854abe50e9358ce4019
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-26 12:14:07 +0400

    add endpoints

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,44 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "London",
+                    Description = "The one with that big clock."
+                },               
+                
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Berlin",
+                    Description = "The one with that big clock."
+                },                
+                
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Sydney",
+                    Description = "The one with that big clock."
+                },                
+                
+                new CityDto()
+                {
+                    Id = 4,
+                    Name = "Moscow",
+                    Description = "The one with that big clock."
+                },
+            };
+        }
+    }
+}
--- CityInfo.API/Controllers/CititesController.cs
+++ CityInfo.API/Controllers/CititesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
@@ -7,14 +8,22 @@ namespace CityInfo.API.Controllers
     public class CititesController : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetCities()
+        public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return new JsonResult(
-                new List<object>
-                {
-                    new {id = 1, Name = "Berlin"},
-                    new {id = 2, Name = "Madrid"}
-                });
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
         }
     }
 }
