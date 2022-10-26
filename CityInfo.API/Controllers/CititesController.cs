fec79723dc20f9c0de5bdbaa44e7779ddbb78277
Kanan Garazada <kanangarayev02@gmail.com>
2022-10-26 11:34:52 +0400

    add cities controller

---
+++ CityInfo.API/Controllers/CititesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CititesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "Berlin"},
+                    new {id = 2, Name = "Madrid"}
+                });
+        }
+    }
+}
